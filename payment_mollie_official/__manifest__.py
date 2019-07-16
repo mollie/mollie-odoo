3ead68b563fb6c96942c1b37aac2151c2843ea83
Ronald Portier <ronald@therp.nl>
2019-07-16 14:17:11 +0200

    [FIX] Prevent runbot WARNING messages.

--- payment_mollie_official/__manifest__.py
+++ payment_mollie_official/__manifest__.py
@@ -1,11 +1,15 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 {
     'name': 'Mollie Payment Acquirer',
     'version': '1.10',
     'author': 'Mollie & BeOpen',
     'website': 'http://www.mollie.com',
     'category': 'eCommerce',
-    'description': "",
+    'description': """
+        Mollie helps businesses of all sizes to sell and build
+         more efficiently with a solid but easy-to-use payment solution.
+         Start growing your business today with effortless payments.
+    """,
     'depends': ['payment','website_sale'],
     'data': [
         'views/mollie.xml',
