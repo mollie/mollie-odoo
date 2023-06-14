fcb5383982411b77287bf9bd0a82712363eef839
odoo-mvds <mv@applix.be>
2023-06-14 14:47:15 +0530

    [FIX] Fix access issue for the pos user group.

--- mollie_pos_terminal/__manifest__.py
+++ mollie_pos_terminal/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Mollie Pos Terminal',
-    'version': '16.0.0.1',
+    'version': '16.0.0.2',
     'description': '',
     'summary': 'Connect your pos with mollie terminal',
     'author': 'Mollie',
