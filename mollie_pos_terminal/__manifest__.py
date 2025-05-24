28823c1664797622f610eccc554504efb1046eb8
Parth Gajjar <parthegajjar@gmail.com>
2025-05-24 14:34:24 +0530

    [IMP] mollie_pos_terminal: released v16.0.0.5
    - Added pagination support for terminal list sync

--- mollie_pos_terminal/__manifest__.py
+++ mollie_pos_terminal/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Mollie Pos Terminal',
-    'version': '16.0.0.4',
+    'version': '16.0.0.5',
     'description': '',
     'summary': 'Connect your pos with mollie terminal',
     'author': 'Mollie',
