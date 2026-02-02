8f51f89484d06cfa0a540a597febae29d84d847a
Parth Gajjar <parthegajjar@gmail.com>
2026-02-02 15:06:40 +0530

    [IMP] mollie_pos_terminal_self: Automatically assigns the default customer to POS orders created in Kiosk

--- mollie_pos_terminal_self/__manifest__.py
+++ mollie_pos_terminal_self/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Mollie Pos Terminal Kiosk (self order)',
-    'version': '18.0.0.0',
+    'version': '18.0.0.1',
     'summary': "Addon for the self order app that allows customers to pay by Mollie terminal.",
     'author': 'Mollie',
     'category': 'Point of Sale',
@@ -8,7 +8,7 @@
     'license': 'LGPL-3',
     'auto_install': True,
     'depends': [
-        'mollie_pos_terminal', 'pos_self_order'
+        'mollie_pos_terminal', 'pos_self_order',
     ],
     'images': [
         'static/description/cover.png',
@@ -17,13 +17,6 @@
         'pos_self_order.assets': [
             'mollie_pos_terminal_self/static/src/app/self_order_service.js',
         ]
-    }
+    },
 }
 
-
-# error from
-# /home/droggol/odoo/community/addons/pos_self_order/static/src/app/self_order_service.js line: 709
-# updateOrderFromServer(order) {
-#     this.currentOrder.updateDataFromServer(order);
-# }
-# 'updateDataFromServer' method not found
--- mollie_pos_terminal_self/models/pos_payment_method.py
+++ mollie_pos_terminal_self/models/pos_payment_method.py
@@ -26,5 +26,14 @@ class PosPaymentMethod(models.Model):
     def _load_pos_self_data_domain(self, data):
         domain = super()._load_pos_self_data_domain(data)
         if data['pos.config']['data'][0]['self_ordering_mode'] == 'kiosk':
-            domain = expression.OR([[('use_payment_terminal', '=', 'mollie')], domain])
+            domain = expression.OR([[('use_payment_terminal', '=', 'mollie'), ('id', 'in', data['pos.config']['data'][0]['payment_method_ids'])], domain])
         return domain
+
+class PosOrder(models.Model):
+    _inherit = 'pos.order'
+
+    def _action_set_partner(self, payment_method_id):
+        payment_method = self.env['pos.payment.method'].sudo().browse(payment_method_id)
+        if payment_method.use_payment_terminal == 'mollie' and payment_method.split_transactions and payment_method.mollie_payment_default_partner:
+            self.sudo().write({'partner_id': payment_method.mollie_payment_default_partner.id})
+        return True
