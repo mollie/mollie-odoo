21e441a32efdcf4455b7a7ab83bbac9c020ff73c
Parth Gajjar <parthegajjar@gmail.com>
2026-02-05 19:08:22 +0530

    [IMP] mollie_balance_sync: Enhanced API error handling

--- mollie_balance_sync/models/account_journal.py
+++ mollie_balance_sync/models/account_journal.py
@@ -240,9 +240,13 @@ class AccountJournal(models.Model):
         self.ensure_one()
         endpoint = f'/v2/{endpoint.strip("/")}'
         url = urls.url_join('https://api.mollie.com/', endpoint)
+        key = self._get_mollie_api_key()
+        if not key:
+            _logger.warning("No Mollie API key defined. Skip API Call")
+            return {}
         headers = {
             'content-type': 'application/json',
-            'Authorization': self._get_mollie_api_key()
+            'Authorization': key
         }
         _logger.info('Mollie SYNC CALL on: %s', endpoint)
 
