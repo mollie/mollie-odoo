b6e90ddda332385ed11f0d2108e923ccb42eaf8b
Parth Gajjar <parthegajjar@gmail.com>
2026-07-08 19:13:04 +0530

    [IMP] Update Mollie Pos Terminal to v15.0.0.3 - Improvement and fixes

--- mollie_pos_terminal/__manifest__.py
+++ mollie_pos_terminal/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Mollie Pos Terminal',
-    'version': '15.0.0.2',
+    'version': '15.0.0.3',
     'description': '',
     'summary': 'Connect your pos with mollie terminal',
     'author': 'Mollie',
